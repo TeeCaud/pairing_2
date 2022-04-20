39d145aa0904430c666920896de5c72889c20fd6
Tyler Caudill <teecaud@hotmail.com>
2022-04-20 18:06:32 -0500

    add places to visit

--- friends.rb
+++ friends.rb
@@ -2,3 +2,7 @@ arrays=["kate", "andy", "kiara", "evan", "nate"]
 puts arrays[2]
 puts arrays[4]
 puts arrays[0]
+
+places_to_visit=["maine", "minnesota", "michigan", "montana", "mississippi"]
+places_to_visit.unshift("nebraska")
+puts places_to_visit
